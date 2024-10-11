1e25a2cd6d990c8cc408e09d0d727b5aa7e2d524
Giovanni Molina <giovanni.m.05@gmail.com>
2024-10-10 21:47:26 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <Header />
+    <div class="instrumentos">
+      <div v-for="instrumento in instrumentos">
+        <Instrumentos :foto="instrumento.foto" :nombre="instrumento.nombre" :precio="instrumento.precio" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Instrumentos from './components/Instrumentos.vue'
+
+export default {
+  components: {
+    Header,
+    Instrumentos
+  },
+  data() {
+    return {
+      instrumentos: [
+        {
+          foto: "https://cdnx.jumpseller.com/lucy-rock/image/43497740/resize/800/1000?1702409652",
+          nombre: "Bateria",
+          precio: "$150.000",
+        },
+        {
+          foto: "https://www.importone.cl/wp-content/uploads/2024/02/208867-1.webp",
+          nombre: "Ibanez JEM",
+          precio: "$650.000",
+        },
+        {
+          foto: "https://www.ebest.cl/media/catalog/product/cache/47abc4af9d81a631bd44d97ba9797770/6/1/610_3.png",
+          nombre: "Teclado Casio",
+          precio: "$210.000",
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+.instrumentos {
+  width: 80%;
+  margin: 30px auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+}
+</style>
---
+++ src/components/Instrumentos.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="instrumento">
+        <div :style="{ backgroundImage: `url(${foto})` }" class="avatar"></div>
+        <h2>{{ nombre }}</h2>
+        <p>{{ precio }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["foto", "nombre", "precio"],
+}
+</script>
+
+<style scoped>
+.instrumento {
+    margin-top: 10px;
+    text-align: center;
+    width: fit-content;
+}
+
+.avatar {
+    background-size: cover;
+    background-position: center;
+    height: 200px;
+    width: 200px;
+    border-radius: 50%;
+}
+</style>
